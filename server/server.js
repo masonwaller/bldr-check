95feacc618fecf5d590f6e09159ecc4233b301a5
Mason Waller <masonwaller805@gmail.com>
2025-02-25 12:41:17 -0700

    remove unnecessary comments

--- server/server.js
+++ server/server.js
@@ -1,18 +1,3 @@
-// const express = require("express");
-// const cors = require("cors");
-
-// const app = express();
-// app.use(cors());
-// const port = 8080;
-
-// app.get("/", (req, res) => {
-//   res.send("Hello from the backend!");
-// });
-
-// app.listen(port, () => {
-//   console.log(`Server running on http://localhost:${port}`);
-// });
-
 const WebSocket = require("ws");
 const server = new WebSocket.Server({ port: 8080 });
 
